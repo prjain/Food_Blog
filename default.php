48cb45b2e61ef30a5cbf9c1141ac9f4960745a3f
Prachi Jain <prachi.mmc@gmail.com>
2013-04-04 23:02:19 +1100

    add all files

---
+++ default.php
@@ -0,0 +1,14 @@
+<html>
+	<body>
+
+		<?php
+		function writeName($fname) {
+		echo $fname." Jain.<br>";
+		}
+		echo "My name is ";
+		writeName("Prachi ");
+		echo "My brother's name is ";
+		writeName("Priyesh");
+	?>
+	</body>
+</html>
